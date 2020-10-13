b1c0b699dd9343be9aac0fbe6f98493d9c5d61cd
MrGhost23 <72778139+MrGhost23@users.noreply.github.com>
2020-10-13 23:15:50 +0200

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -5,18 +5,18 @@ module.exports.run = (client, message, args) => {
     if (!mesg) return message.channel.send("Please say something.");
     
     // part one
-     fetch(`http://api.brainshop.ai/get?bid=153646&key=Z1gSIIkZ9W1lXOQF&uid=1&msg=${encodeURIComponent(mesg)}`)
-     .then(res => res.json())
-     .then(data => {
-          message.channel.send(data.cnt);
-     });
+     //fetch(`http://api.brainshop.ai/get?bid=153646&key=Z1gSIIkZ9W1lXOQF&uid=1&msg=${encodeURIComponent(mesg)}`)
+     //.then(res => res.json())
+     //.then(data => {
+      //    message.channel.send(data.cnt);
+     //});
     
     // part two
-    // fetch(`https://some-random-api.ml/chatbot?message=${encodeURIComponent(mesg)}`)
-    // .then(res => res.json())
-    // .then(data => {
-    //    message.channel.send(data.response);
-    // });
+     fetch(`https://some-random-api.ml/chatbot?message=${encodeURIComponent(mesg)}`)
+     .then(res => res.json())
+     .then(data => {
+        message.channel.send(data.response);
+     });
 }
 
 module.exports.help = {
